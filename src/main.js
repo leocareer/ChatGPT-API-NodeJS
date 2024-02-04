a868c7fb366e974346f91ef97b4acc98c59d16e8
leocareer <leo.ph.career@gmail.com>
2024-02-04 22:17:14 +0100

    text message

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log('working..!.')
+import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
+import config from 'config'
+
+const bot = new Telegraf(config.get('telegram_token'))
+
+bot.on(message('text'), async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message, null, 2))
+})
+
+bot.command('start', async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message, null, 2))
+})
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
